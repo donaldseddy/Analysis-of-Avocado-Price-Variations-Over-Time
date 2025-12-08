571ab270b9d436fc4e7f4e652eeca1396c20bfaa
Your Name <you@example.com>
2025-12-08 11:40:26 +0100

    Refactor forecasting functions to accept dynamic label parameter
    - Updated `naive_forecast_model`, `seasonal_naive_forecast_model`, and `moving_average_forecast_model` to accept a `label` parameter, allowing for flexibility in selecting the target column for forecasting.
    - Replaced hardcoded 'visits' references with the `label` parameter in each function.

--- src/fonctions.py
+++ src/fonctions.py
@@ -8,21 +8,21 @@ import re
 
 
 
-def naive_forecast_model(train, test):
+def naive_forecast_model(train, test,label='visits'):
     """Generate naive forecasts by repeating the last observed value."""
-    naive_forecast = train['visits'].iloc[-1]
+    naive_forecast = train[label].iloc[-1]
     naive_forecasts = pd.Series(naive_forecast, index=test.index)
     return naive_forecasts
 
-def seasonal_naive_forecast_model(train, test):
+def seasonal_naive_forecast_model(train, test,label='visits'):
     """Generate seasonal naive forecasts by shifting the series by 7 days."""
-    seasonal_naive_forecasts = train['visits'].shift(7).iloc[-30:]
+    seasonal_naive_forecasts = train[label].shift(7).iloc[-30:]
     seasonal_naive_forecasts.index = test.index
     return seasonal_naive_forecasts
 
-def moving_average_forecast_model(train, test):
+def moving_average_forecast_model(train, test,label='visits'):
     """Generate moving average forecasts with a window of 7 days."""
-    moving_average_forecasts = train['visits'].rolling(window=7).mean().iloc[-30:]
+    moving_average_forecasts = train[label].rolling(window=7).mean().iloc[-30:]
     moving_average_forecasts.index = test.index
     return moving_average_forecasts
 
